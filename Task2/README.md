5122301bacd6edf4a21a513d82cff26811a86475
annah11 <hanamesfin67@gmail.com>
2024-08-08 09:06:12 +0300

    initial commit

---
+++ Task2/README.md
@@ -0,0 +1,34 @@
+<<<<<<< HEAD
+# Todo-List
+=======
+# Todo List App
+
+This is a simple Todo List application built using HTML, CSS, and TypeScript.
+
+## Features
+
+- Add new tasks
+- Delete tasks
+- List tasks
+- edit tasks
+- save tasks
+
+## How to Run
+
+1. Clone the repository to your local machine.
+     ```sh
+   git clone https://github.com/annah11/Todo-List/Task2.git
+2. Open the `index.html` file in your web browser.
+
+## Screenshots
+
+### Main Screen
+![Main Screen](screenshot1.png)
+
+### Adding a Task
+![Adding a Task](screenshot2.png)
+
+### Task Added
+![Task Added](screenshot3.png)
+
+>>>>>>> bbea5c7 (Initial commit)
---
+++ Task2/script.js
@@ -0,0 +1,53 @@
+window.addEventListener('load', function () {
+    var form = document.querySelector("#new-task-form");
+    var input = document.querySelector("#new-task-input");
+    var list_el = document.querySelector("#tasks");
+    form === null || form === void 0 ? void 0 : form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        var task = input === null || input === void 0 ? void 0 : input.value.trim();
+        if (!task) {
+            alert("Please fill out the task.");
+            return;
+        }
+        var task_el = document.createElement('div');
+        task_el.classList.add('task');
+        var task_content_el = document.createElement('div');
+        task_content_el.classList.add('content');
+        var task_input_el = document.createElement('input');
+        task_input_el.classList.add('text');
+        task_input_el.type = 'text';
+        task_input_el.value = task;
+        task_input_el.setAttribute('readonly', 'readonly');
+        task_content_el.appendChild(task_input_el);
+        var task_actions_el = document.createElement('div');
+        task_actions_el.classList.add('actions');
+        var task_edit_el = document.createElement('button');
+        task_edit_el.classList.add('edit');
+        task_edit_el.innerText = 'Edit';
+        var task_delete_el = document.createElement('button');
+        task_delete_el.classList.add('delete');
+        task_delete_el.innerText = 'Delete';
+        task_actions_el.appendChild(task_edit_el);
+        task_actions_el.appendChild(task_delete_el);
+        task_el.appendChild(task_content_el);
+        task_el.appendChild(task_actions_el);
+        list_el === null || list_el === void 0 ? void 0 : list_el.appendChild(task_el);
+        if (input) {
+            input.value = '';
+        }
+        task_edit_el.addEventListener('click', function () {
+            if (task_edit_el.innerText.toLowerCase() === "edit") {
+                task_edit_el.innerText = "Save";
+                task_input_el.removeAttribute("readonly");
+                task_input_el.focus();
+            }
+            else {
+                task_edit_el.innerText = "Edit";
+                task_input_el.setAttribute("readonly", "readonly");
+            }
+        });
+        task_delete_el.addEventListener('click', function () {
+            list_el === null || list_el === void 0 ? void 0 : list_el.removeChild(task_el);
+        });
+    });
+});
